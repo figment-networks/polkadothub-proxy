be9773bb405ae3c13543ae7f381428b3eff5b3ce
Paweł Nguyen <pawel.nguyen@gmail.com>
2020-05-20 16:25:33 +0200

    Add error handling console.error to client.js

--- client.js
+++ client.js
@@ -10,7 +10,7 @@ const {
 } = require('./grpc/init');
 
 function main() {
-  const height = 11;
+  const height = 2003930;
   const address = 'DSpbbk6HKKyS78c4KDLSxCetqbwnsemv2iocVXwNe2FAvWC';
 
   // Block
@@ -24,46 +24,78 @@ function main() {
   });
 
   // client.getHead({}, function(err, response) {
-  //   console.log('head: ', response);
+  //   if (err) {
+  //     console.error(err)
+  //   } else {
+  //     console.log('head: ', response);
+  //   }
   // });
 
-  client.getMetaByHeight({height: height}, function(err, response) {
-    console.log('block meta: ', response);
+  client.getMetaByHeight({height: height}, function (err, response) {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log('block meta: ', response);
+    }
   });
 
-  // // Transactions
+  // Transactions
   // client = new transactionProto.TransactionService('localhost:50051', grpc.credentials.createInsecure());
   // client.getByHeight({height: height}, function(err, response) {
-  //   console.log('transactions: ', response);
+  //   if (err) {
+  //     console.error(err)
+  //   } else {
+  //     console.log('transactions: ', response);
+  //   }
   // });
 
-  // // Events
+  // Events
   // client = new eventProto.EventService('localhost:50051', grpc.credentials.createInsecure());
   // client.getByHeight({height: height}, function(err, response) {
-  //   console.log('events: ', response.events);
+  //   if (err) {
+  //     console.error(err)
+  //   } else {
+  //     console.log('events: ', response.events);
+  //   }
   // });
 
   // Staking
   // client = new stakingProto.StakingService('localhost:50051', grpc.credentials.createInsecure());
   // client.getByHeight({height: height}, function (err, response) {
-  //   console.log('staking: ', JSON.stringify(response.staking));
+  //   if (err) {
+  //     console.error(err)
+  //   } else {
+  //     console.log('staking: ', JSON.stringify(response.staking));
+  //   }
   // });
 
   // Account Identity
   // client = new accountProto.AccountService('localhost:50051', grpc.credentials.createInsecure());
   // client.getIdentity({address}, function(err, response) {
-  //   console.log('account identity: ', response.identity);
+  //   if (err) {
+  //     console.error(err)
+  //   } else {
+  //     console.log('account identity: ', response.identity);
+  //   }
   // });
 
   // Account
   // client.getByHeight({height, address}, function(err, response) {
-  //   console.log('account: ', response.account);
+  //   if (err) {
+  //     console.error(err)
+  //   } else {
+  //     console.log('account: ', response.account);
+  //   }
   // });
 
   // Validator Performance
   // client = new validatorPerformanceProto.ValidatorPerformanceService('localhost:50051', grpc.credentials.createInsecure());
   // client.getByHeight({height: height}, function(err, response) {
-  //   console.log('validator performance: ', response);
+  //   if (err) {
+  //     console.error(err)
+  //   } else {
+  //     console.log('validator performance: ', response);
+  //   }
   // });
 }
 
