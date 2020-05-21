93f003c9f8b6efc4b732f717a6625374ea62a173
Paweł Nguyen <pawel.nguyen@gmail.com>
2020-05-21 15:31:33 +0200

    Add an integration test for validator_performance

---
+++ handlers/validator_performance_handlers.test.js
@@ -0,0 +1,19 @@
+const validatorPerformanceHandlers = require('./validator_performance_handlers');
+const {createApi} = require('../tests/shared');
+
+// These actually DO hit the API
+describe('integration', () => {
+  let call = {request: {height: 2197577}};
+  let api;
+
+  beforeAll(async (done) => {
+    api = await createApi();
+    done()
+  });
+
+  describe('getByHeight', () => {
+    it('returns a block that matches snapshot', async () => {
+      expect(await validatorPerformanceHandlers.getByHeight(api, call)).toMatchSnapshot();
+    });
+  });
+});
