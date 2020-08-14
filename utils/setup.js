3d9547d7844117eb619b558245563acb145ea525
Bartosz Hejman <bartoszhejman@gmail.com>
2020-08-14 18:02:03 +0200

    Remove setupApiAtHeight

--- utils/setup.js
+++ utils/setup.js
@@ -10,30 +10,6 @@ const metadataCache = {
 Since Polkadot uses WebAssembly, metadata and types can be different at different height.
 We need to inject appropriate metadata and types to make sure that they are valid for given height.
  */
-const setupApiAtHeight = async (api, height) => {
-  const blockHash = await api.rpc.chain.getBlockHash(height);
-  const runtimeVersionAt = await api.rpc.state.getRuntimeVersion(blockHash);
-
-  let metaDataAt;
-  if (metadataCache[runtimeVersionAt.specVersion]) {
-    metaDataAt = metadataCache[runtimeVersionAt.specVersion];
-  } else {
-    metaDataAt = await api.rpc.state.getMetadata(blockHash);
-  }
-
-  const chain = await api.rpc.system.chain();
-  const types = getSpecTypes(api.registry, chain, runtimeVersionAt.specName, runtimeVersionAt.specVersion);
-
-  api.injectMetadata(metaDataAt);
-  api.registerTypes(types);
-
-  return {
-    chain,
-    specVersion: runtimeVersionAt.specVersion,
-    blockHash,
-  };
-}
-
 const fetchMetadataAtHeight = async (api, height) => {
   const blockHash = await api.rpc.chain.getBlockHash(height);
   const runtimeVersionAt = await api.rpc.state.getRuntimeVersion(blockHash);
@@ -76,7 +52,6 @@ const injectMetadata = (api, data) => {
 }
 
 module.exports = {
-  setupApiAtHeight,
   fetchMetadataAtHeight,
   injectMetadata,
 };
