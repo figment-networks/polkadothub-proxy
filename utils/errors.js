f817d585be8acc3966c05c95999397992031c5e2
eesmerdag <emiresmerdag@gmail.com>
2020-10-07 13:57:56 +0200

    Error messages to rollbar

--- utils/errors.js
+++ utils/errors.js
@@ -1,10 +1,12 @@
 const grpc = require('grpc');
+const {rollbar} = require('./rollbar');
 
 class ProxyBaseError extends Error {
   constructor(message) {
     super(message);
     this.code = grpc.status.UNKNOWN;
     Error.captureStackTrace(this, this.constructor);
+    rollbar.error("msg: " + this.message + " stacktrace: " + this.stack)
   }
 }
 
