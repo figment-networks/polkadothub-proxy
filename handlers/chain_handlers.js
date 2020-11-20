8ff88876b3552e28c1465b1683b669748aa9ecea
Jessica Paczuski <jessica@figment.network>
2020-11-19 21:39:51 -0500

    Check height on chain metadata call

--- handlers/chain_handlers.js
+++ handlers/chain_handlers.js
@@ -62,8 +62,11 @@ const getStatus = async (api, _call, context = {}) => {
  */
 const getMetaByHeight = async (api, call, context = {}) => {
   const height = parseInt(call.request.height, 10);
-  const prevBlockHash = context.prevBlockHash ? context.prevBlockHash : await getHashForHeight(api, height-1);
+
   const blockHash = context.blockHash ? context.blockHash : await getHashForHeight(api, height);
+  const prevBlockHash = height > 1 ?
+    (context.prevBlockHash ? context.prevBlockHash : await getHashForHeight(api, height-1))
+    : blockHash
 
   const [chain, runtimeVersionAt, rawCurrentEra, rawCurrentSession, rawNextEra, rawNextSession, rawTimestampAt
   ] = await Promise.all([
