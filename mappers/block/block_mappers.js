1de3a1ef6dc26796b8152009059b523458751f82
Łukasz Miłkowski <lukanus@uaznia.net>
2021-05-25 20:01:24 +0200

    Fix for fee

--- mappers/block/block_mappers.js
+++ mappers/block/block_mappers.js
@@ -1,9 +1,11 @@
 const transactionMappers = require('../transaction/transaction_mappers');
+const eventMappers = require('../event/event_mappers');
 
-const toPb = (blockHash, blockHeight, rawHeader, rawExtrinsic, rawTimestamp, rawEvents, rawCurrentEra, calcFee) => {
+
+const toPb = (blockHash, blockHeight, rawHeader, rawExtrinsic, rawTimestamp, rawEvents, rawCurrentEra, calcFee, baseWeight) => {
   rawEvents.forEach((ev, i) => ev.index = i);
   const currentEra = rawCurrentEra.toString();
-  
+
   return {
     block: {
       blockHash: blockHash.toString(),
@@ -16,11 +18,44 @@ const toPb = (blockHash, blockHeight, rawHeader, rawExtrinsic, rawTimestamp, raw
       },
       extrinsics: rawExtrinsic.map((rawExtrinsic, index) => {
         const rawEventsForExtrinsic = rawEvents.filter((ev) => ev.phase.isApplyExtrinsic && ev.phase.asApplyExtrinsic.toNumber() === index);
-        return transactionMappers.toPb(index, rawExtrinsic, rawTimestamp, rawEventsForExtrinsic, calcFee, currentEra);
+        const events = rawEventsForExtrinsic.map((rawEvent) => ({
+          index: rawEvent.index,
+          error: rawEvent.error,
+          ...eventMappers.toPb(rawEvent)
+         }));
+         const partialFee = getPartialFee(rawExtrinsic, events, calcFee, baseWeight);
+
+        return transactionMappers.toPb(index, rawExtrinsic, rawTimestamp, rawEventsForExtrinsic, partialFee, currentEra, events);
       }),
     }
   };
 };
+
+
+const getPartialFee = (rawExtrinsic, events, calcFee, baseWeight) => {
+    // Polkadot doesn't charge a fee for unsigned transactions https://wiki.polkadot.network/docs/en/learn-transaction-fees
+    if (!rawExtrinsic.isSigned || !calcFee) {
+        return
+    }
+
+    const completedEvent = events.find(({ section, method }) =>
+        section == 'system' && (method === 'ExtrinsicSuccess' || method === 'ExtrinsicFailed')
+    );
+
+    const dispatchInfo = completedEvent && completedEvent.data && completedEvent.data.find(({ name }) => name == 'DispatchInfo');
+    if (!dispatchInfo) {
+        return
+    }
+
+    const paysFee = JSON.parse(dispatchInfo.value).paysFee;
+    const weight = JSON.parse(dispatchInfo.value).weight;
+
+    if (!weight || !(paysFee === "Yes" || paysFee === true)) {
+        return
+    }
+
+    return calcFee.calc_fee(BigInt(weight.toString()), rawExtrinsic.encodedLength, baseWeight);
+}
 
 module.exports = {
   toPb,
